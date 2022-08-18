d58fc79e3d4b86d889c3569fb17d89db760ced15
Juan Moreno <chuanmi@icloud.com>
2022-08-18 17:04:46 +0200

    modified:   04_lists.py

--- 04_lists.py
+++ 04_lists.py
@@ -19,6 +19,16 @@ print(my_other_list[0])
 print(my_other_list[1])
 print(my_other_list[-1])
 print(my_other_list[-4])
+print(my_list.count(30))
 # print(my_other_list[4]) IndexError
 # print(my_other_list[-5]) IndexError
+
+age, height, name, surname = my_other_list
+print(name)
+
+print(my_list+my_other_list)
+
+my_list = "Hola Python"
+print(my_list)
+print(type(my_list))
 
