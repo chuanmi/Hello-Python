e936b631b71af530fa75b197425a4b72b09a70be
Juan Moreno <chuanmi@icloud.com>
2022-08-18 16:06:54 +0200

    04_lists.py

---
+++ 04_lists.py
@@ -0,0 +1,15 @@
+### Lists ###
+
+my_list = list()
+my_other_list =[]
+
+print(len(my_list))
+
+my_list = [35, 24, 62, 52, 30, 30, 17]
+
+print (my_list)
+print(len(my_list))
+
+my_other_list = [25, 1.77, "chuanmi", "Moreno"]
+
+print(type(my_list))
