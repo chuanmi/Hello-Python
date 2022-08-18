33a78119a9641054fbbccdc65ba899f356fe29d3
Juan Moreno <chuanmi@icloud.com>
2022-08-18 18:05:48 +0200

    modified:   05_tuples.py

--- 05_tuples.py
+++ 05_tuples.py
@@ -3,3 +3,11 @@
 my_tuple = tuple()
 my_other_tuple = ()
 
+my_tuple = (35, 1.77, "chuanmi", "Moreno")
+print(my_tuple)
+print(type(my_tuple))
+
+print(my_tuple[0])
+print(my_tuple[-1])
+# print(my_tuple[4]) IndexError
+# print(my_tuple[-6]) IndexError
