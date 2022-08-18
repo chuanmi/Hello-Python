589156918a122c79515683d51d149db38af2eb6a
Juan Moreno <chuanmi@icloud.com>
2022-08-18 18:31:14 +0200

    modified:   05_tuples.py

--- 05_tuples.py
+++ 05_tuples.py
@@ -3,7 +3,9 @@
 my_tuple = tuple()
 my_other_tuple = ()
 
-my_tuple = (35, 1.77, "chuanmi", "Moreno")
+my_tuple = (35, 1.77, "chuanmi", "Moreno", "chuanmi")
+my_other_tuple = (35, 60, 30)
+
 print(my_tuple)
 print(type(my_tuple))
 
@@ -11,3 +13,14 @@ print(my_tuple[0])
 print(my_tuple[-1])
 # print(my_tuple[4]) IndexError
 # print(my_tuple[-6]) IndexError
+
+print(my_tuple.count("chuanmi"))
+print(my_tuple.index("Moreno"))
+print(my_tuple.index("chuanmi"))
+
+# my_tuple[1] = 1.80 'tuple' object does not support item assignment
+
+my_sum_tuple = my_tuple + my_other_tuple
+print(my_sum_tuple)
+
+print(my_sum_tuple[3:6])
