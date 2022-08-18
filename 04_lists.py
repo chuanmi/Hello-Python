f262c4380cf66376fe88f32270bd8988a22dd738
Juan Moreno <chuanmi@icloud.com>
2022-08-18 16:39:49 +0200

    modified:   04_lists.py

--- 04_lists.py
+++ 04_lists.py
@@ -10,6 +10,15 @@ my_list = [35, 24, 62, 52, 30, 30, 17]
 print (my_list)
 print(len(my_list))
 
-my_other_list = [25, 1.77, "chuanmi", "Moreno"]
+my_other_list = [41, 1.78, "chuanmi", "Moreno"]
 
 print(type(my_list))
+print(type(my_other_list))
+
+print(my_other_list[0])
+print(my_other_list[1])
+print(my_other_list[-1])
+print(my_other_list[-4])
+# print(my_other_list[4]) IndexError
+# print(my_other_list[-5]) IndexError
+
